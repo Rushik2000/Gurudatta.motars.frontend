ca11f716605c16d40ce95c16ead6f95b8a47d200
Rushi <kalerushi200@gmail.com>
2025-10-28 16:15:56 +0530

    Dashboard update

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,21 +4,15 @@ import '../App.css'
 import bill from '../assets/bill.png'
 import customer from '../assets/customer.png'
 import inventory from '../assets/inventory.png'
+import user_1 from '../assets/user_1.png'
 
 const Dashboard = () => {
   return (
     <>
-      <header className='app-header'>
-        <div className='name-logo-header'>
-          <h2>Gurudatta app</h2>
-        </div>
-        <nav>
-          <h2>Profile icon + User</h2>
-        </nav>
-      </header>
-
-      <div>
+      <div className='header-grid'>
+        <h2></h2>
         <img src={logo} className="logo react" alt="React logo" />
+        <img src={user_1} className='profile-icon' />
       </div>
 
       <div className="card-grid">
